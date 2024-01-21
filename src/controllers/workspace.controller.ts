93882ff56717faf83f4861f0725c5b15adc16d77
Ahd-3hd <ahdhani0@gmail.com>
2024-01-21 14:17:18 +0300

    feat: create workspace

---
+++ src/controllers/workspace.controller.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import { WorkspaceModel } from "../models/Workspace.model";
+
+export const createWorkspace = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { userid } = req.headers;
+    const { name } = req.body;
+
+    console.log(userid);
+
+    const workspace = await WorkspaceModel.create({
+      name,
+      owner: parseInt(userid as string),
+    });
+
+    res.status(201).json({
+      workspace,
+      message: "Workspace created",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routers/workspace.router.ts
+++ src/routers/workspace.router.ts
@@ -1,6 +1,10 @@
 import { Router } from "express";
-import { validate } from "../middlewares";
+import { validate } from "../middlewares/validate";
+import { createWorkspace } from "../controllers/workspace.controller";
+import { createWorkspaceSchema } from "../middlewares/schemas/createWorkspace.schema";
 
 const router = Router();
+
+router.post("", validate(createWorkspaceSchema), createWorkspace);
 
 export default router;
