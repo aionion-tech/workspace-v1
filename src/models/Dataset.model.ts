f25629b88185c7126aef5524430fbfa71aacba2a
Ahd-3hd <ahdhani0@gmail.com>
2024-01-21 14:45:30 +0300

    feat: create project and dataset models

---
+++ src/models/Dataset.model.ts
@@ -0,0 +1,50 @@
+import sequelize from "../db/connect";
+import { DataTypes, Model } from "sequelize";
+import { WorkspaceModel } from "./Workspace.model";
+import { Dataset } from "../interfaces/Dataset.interface";
+import { ProjectModel } from "./Project.model";
+
+class DatasetClass extends Model<Dataset> implements Dataset {
+  public id!: number;
+  public name!: string;
+  public workspace!: number;
+  public project!: number;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+const DatasetModel = DatasetClass.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    workspace: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: WorkspaceModel,
+        key: "id",
+      },
+    },
+    project: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: ProjectModel,
+        key: "id",
+      },
+    },
+  },
+  { tableName: "datasets", sequelize }
+);
+
+DatasetModel.sync({ force: true });
+
+export { DatasetModel };
---
+++ src/models/Project.model.ts
@@ -0,0 +1,40 @@
+import sequelize from "../db/connect";
+import { DataTypes, Model } from "sequelize";
+import { Project } from "../interfaces/Project.interface";
+import { WorkspaceModel } from "./Workspace.model";
+
+class ProjectClass extends Model<Project> implements Project {
+  public id!: number;
+  public name!: string;
+  public workspace!: number;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+const ProjectModel = ProjectClass.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    workspace: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: WorkspaceModel,
+        key: "id",
+      },
+    },
+  },
+  { tableName: "projects", sequelize }
+);
+
+ProjectModel.sync({ force: true });
+
+export { ProjectModel };
